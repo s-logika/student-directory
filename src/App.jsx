053a26b4ebef12c63922bef287cf668c3707329b
s-logika <273270343+s-logika@users.noreply.github.com>
2026-05-02 21:36:12 +0530

    Task 3: Add student data array and pass props to StudentList and StudentCard

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,50 @@ import Header from "./components/Header";
 import StudentList from "./components/StudentList";
 import Footer from "./components/Footer";
 
-const students = [];
+const students = [
+    {
+        id: 1,
+        name: "Amal Perera",
+        age: 21,
+        course: "Information Technology",
+        isActive: true,
+    },
+    {
+        id: 2,
+        name: "Nimesha Silva",
+        age: 22,
+        course: "Business Management",
+        isActive: false,
+    },
+    {
+        id: 3,
+        name: "Kasun Fernando",
+        age: 20,
+        course: "Engineering",
+        isActive: true,
+    },
+    {
+        id: 4,
+        name: "Dilani Ranasinghe",
+        age: 23,
+        course: "Information Technology",
+        isActive: true,
+    },
+    {
+        id: 5,
+        name: "Tharindu Wijesinghe",
+        age: 21,
+        course: "Business Management",
+        isActive: false,
+    },
+    {
+        id: 6,
+        name: "Sanduni Jayawardena",
+        age: 22,
+        course: "Engineering",
+        isActive: true,
+    },
+];
 
 function App() {
     return (
